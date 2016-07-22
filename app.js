efddf618b2ca3fdfc1c65d1d7bff60b136a0d729
bronwyndet <wayward@waywardcoffee.com>
2016-07-22 14:52:55 -0700

    Prob 3 done, tested, fixed tests code

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a,b){ //eslint-disable-line
-  var multOutput = a * b;
-  var multMessage = 'The product of ' + a + ' and ' + b + ' is ' + multOutput + '.';
-  return [multOutput, multMessage];
+  var outputTwo = a * b;
+  var messageTwo = 'The product of ' + a + ' and ' + b + ' is ' + outputTwo + '.';
+  return [outputTwo, messageTwo];
 }
 
 // Here is the test for multiply(); uncomment it to run it
@@ -52,11 +52,21 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
+  var sumOne = sum(a,b)[0];
+  var sumTwo = sum(sumOne,c)[0];
 
+  var multOne = multiply(a,b)[0];
+  var multTwo = multiply(multOne,c)[0];
+
+  var messageSumThree = a + ' and ' + b + ' and ' + c + ' sum to ' + sumTwo + '.';
+  var messageMultThree = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multTwo + '.';
+  console.log(messageSumThree);
+  console.log(messageMultThree);
+  return [sumTwo, multTwo, messageSumThree, messageMultThree];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
